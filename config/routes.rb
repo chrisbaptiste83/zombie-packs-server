62530bf940cad535bcff59d00fdf46ab1f51c6a9
Chris Baptiste <chrisbaptiste83@gmail.com>
2020-09-20 23:27:52 -0700

    add routes for user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do 
-  
+
   resources :comments
   resources :tactical_packages
   resources :users
   
+
+  get "session/status", to: "sessions#is_logged_in?"
+  delete "logout/:id", to: "sessions#destroy"
+  post "login", to: "sessions#login"
+
 end
