63a962ea4144b0ea957a26122bd723bc66db9015
Chris Baptiste <chrisbaptiste83@gmail.com>
2020-09-20 23:31:02 -0700

    update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
     has_secure_password  
     has_many :tactical_packages 
     has_many :comments 
+    validates_presence_of :username, :email
+    validates_uniqueness_of :email, :username
+    validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 end
