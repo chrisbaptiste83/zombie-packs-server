9d4b15c029c0bf0e5349e424f10d7f7fce69ddaf
Chris Baptiste <chrisbaptiste83@gmail.com>
2020-09-20 23:44:06 -0700

    generate migration to add image to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord 
-
+    has_one_attached :avatar
     has_secure_password  
     has_many :tactical_packages 
     has_many :comments 
@@ -9,13 +9,7 @@ class User < ApplicationRecord
     before_create :set_default_avatar
 
     def set_default_avatar
-        if self.gender == "Male"
-            self.image = "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSgMTnyu57w3n7MiSYwH99BIzbT-5HoQUA8Jw&usqp=CAU"
-        elsif self.gender == "Female" 
-            self.image = "https://www.pngkit.com/png/detail/55-554008_woman-head-people-avatar-men-icon-png.png"
-        elsif self.gender == "Transgender"
-            self.image = "https://dlp2gfjvaz867.cloudfront.net/product_photos/11302471/locked-hearts-and-hand-grenades-cute-wallpapers-heart-images_original.jpg"
-        end
+        self.image = "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSgMTnyu57w3n7MiSYwH99BIzbT-5HoQUA8Jw&usqp=CAU"
     end
 
 
