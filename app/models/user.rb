2620e861457892563ab1d83149f6f161f6bc4ad6
Chris Baptiste <chrisbaptiste83@gmail.com>
2021-01-19 12:23:27 -0700

    modify user model by removing default avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,14 @@
 class User < ApplicationRecord 
-    has_one_attached :avatar
+    
     has_secure_password  
     has_many :tactical_packages 
     has_many :comments 
     validates_presence_of :username, :email
     validates_uniqueness_of :email, :username
     validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i 
-    before_create :set_default_avatar
+    
 
-    def set_default_avatar
-        self.image = "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSgMTnyu57w3n7MiSYwH99BIzbT-5HoQUA8Jw&usqp=CAU"
-    end
+
 
 
 
