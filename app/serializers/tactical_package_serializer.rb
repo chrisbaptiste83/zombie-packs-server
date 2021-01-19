00c02c9662e722fa5a7a1fd8d3ba6cd99350c32c
Chris Baptiste <chrisbaptiste83@gmail.com>
2021-01-19 12:24:49 -0700

    update tactical package serializer to work with activerecord

--- app/serializers/tactical_package_serializer.rb
+++ app/serializers/tactical_package_serializer.rb
@@ -1,4 +1,11 @@
 class TacticalPackageSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :user_id, :title, :creator, :description, :primary_weapon_name, :primary_weapon_image_url, :secondary_weapon_name, :secondary_weapon_image_url, :lethal_weapon_name, :lethal_weapon_image_url, :tactical_item_name, :tactical_item_image_url
+  include Rails.application.routes.url_helpers 
+  
+  attributes :id, :user_id, :title, :creator, :description, :primary_weapon_name, :secondary_weapon_name, :lethal_weapon_name, :tactical_item_name, :image
+  
+  def image
+    return rails_blob_url(object.avatar)
+  end 
+
 end
