89d34549b77d3172935ffeecca617c3c68b33d47
Chris Baptiste <chrisbaptiste83@gmail.com>
2021-01-27 04:39:55 -0700

    update tactical package serializer for active storage compatibility

--- app/serializers/tactical_package_serializer.rb
+++ app/serializers/tactical_package_serializer.rb
@@ -1,11 +1,10 @@
-class TacticalPackageSerializer
-  include FastJsonapi::ObjectSerializer
+class TacticalPackageSerializer < ActiveModel::Serializer
+  
   include Rails.application.routes.url_helpers 
   
-  attributes :id, :user_id, :title, :creator, :description, :primary_weapon_name, :secondary_weapon_name, :lethal_weapon_name, :tactical_item_name, :image
+  attributes :id, :user_id, :title, :creator, :description, :primary_weapon_name, :secondary_weapon_name, :lethal_weapon_name, :tactical_item_name, :avatar 
   
-  def image
-    return rails_blob_url(object.avatar)
-  end 
-
+  def avatar
+      rails_blob_path(object.avatar , only_path: true) if object.avatar.attached?
+  end
 end
