85cce8bdfe6e1e57f546f530ff1579f3a428d84a
Chris Baptiste <chrisbaptiste83@gmail.com>
2021-01-27 04:38:51 -0700

    update database schema to no longer have an image column for tactical packages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_11_025317) do
+ActiveRecord::Schema.define(version: 2020_12_11_025210) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,7 +56,6 @@ ActiveRecord::Schema.define(version: 2020_12_11_025317) do
     t.integer "comments_count"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "image"
   end
 
   create_table "users", force: :cascade do |t|
