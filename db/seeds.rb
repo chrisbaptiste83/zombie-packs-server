a249c219d234614a90acaf5b9730b88ca5162a93
Chris Baptiste <chrisbaptiste83@gmail.com>
2021-01-27 04:35:16 -0700

    update seed file to use active storage

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,14 +7,31 @@ User.create(username:"Darryl", password:"darryl00", password_confirmation:"darry
 
 tactical_packages = [ 
 
-    ["Zombie Machine Gunner", "Tactical package with high capacity round machine gun, three round burst automatic pistol, and plenty of explosives.", "Michael", "1", "Hades", "RK 7 Garrison", "Semtex", "Stim", "https://oyster.ignimgs.com/mediawiki/apis.ign.com/call-of-duty-modern-warfare/9/93/Call_of_Duty%C2%AE_Modern_Warfare%C2%AE_20191024221332.jpg"],
+    ["Zombie Machine Gunner", "Tactical package with high capacity round machine gun, three round burst automatic pistol, and plenty of explosives.", "Michael", "1", "Hades", "RK 7 Garrison", "Semtex", "Stim"],
 
-    ["Sniper", "Tactical package with long range, high-caliber sniper rifle, semi-automatic 12 gauge shotgun, and a claymore mine for defense.", "Chris", "2", "Paladin HB50", "SG12", "Claymore", "Heartbeat Sensor", "https://oyster.ignimgs.com/mediawiki/apis.ign.com/call-of-duty-modern-warfare/9/93/Call_of_Duty%C2%AE_Modern_Warfare%C2%AE_20191024221332.jpg"]
+    ["Sniper", "Tactical package with long range, high-caliber sniper rifle, semi-automatic 12 gauge shotgun, and a claymore mine for defense.", "Chris", "2", "Paladin HB50", "SG12", "Claymore", "Heartbeat Sensor"]
 
 ]
 
 tactical_packages.each do |tactical_package|
-    TacticalPackage.create( title: tactical_package[0], description: tactical_package[1], creator: tactical_package[2], user_id: tactical_package[3], primary_weapon_name: tactical_package[4], secondary_weapon_name: tactical_package[5], lethal_weapon_name: tactical_package[6], tactical_item_name: tactical_package[7], image: tactical_package[8])
-  end
+    TacticalPackage.create( title: tactical_package[0], description: tactical_package[1], creator: tactical_package[2], user_id: tactical_package[3], primary_weapon_name: tactical_package[4], secondary_weapon_name: tactical_package[5], lethal_weapon_name: tactical_package[6], tactical_item_name: tactical_package[7])
+  end  
+
+  sniper_package = TacticalPackage.all.first 
+
+  sniper_package.avatar.attach(
+      io: File.open('./public/images/weapons_class.jpg'), 
+      filename: 'weapons_class.jpg', 
+      content_type: 'application/jpg'
+  ) 
+
+  shotgun_package =  TacticalPackage.all.last 
+
+  shotgun_package.avatar.attach(
+      io: File.open( './public/images/shotgun.jpg'), 
+      filename: 'shotgun.jpg', 
+      content_type: 'application/jpg'  
+    )
+
 
 
